ab02d71af30e78d32eaa486c52133b106d8e0d4e
ya107231@gmail.com <ya107231@gmail.com>
2023-02-22 19:57:07 +0200

    Editing range

--- src/range.ts
+++ src/range.ts
@@ -2,7 +2,7 @@ import createErrorThrower from "./create-error-thrower.js";
 
 const throwError = createErrorThrower(range);
 
-/** ``range`` is a function that returns an array of number depending on `to` and `from` parameters */
+/** ``range`` is a function that returns an array of number depending on `to` and `from` parameters. */
 function range(to: number, from: number = 0): number[] {
   if (!to) throwError("\"to\" parameter is required!");
   if (typeof to !== "number" || (from && typeof from !== "number")) throwError("\"range\" accepts numbers only!");
