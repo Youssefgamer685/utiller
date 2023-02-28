f9caeec1b2dd429f8007d7bd9292f826ab05fa3a
Youssefgamer685 <ya107231@gmail.com>
2023-02-28 13:27:01 +0200

    delete: index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const { querystring } = require("utiller");
-
-console.log(querystring.decode("first_name=Youssef&age=14"));
-console.log(querystring.decode("first_name=Youssef&age=14", { camelCase: false }));
-console.log(querystring.decode("first_name:Youssef,age:14", { camelCase: false, equalOperator: ":", separator: "," }));
-console.log(querystring.decode("first_name=Youssef,age=14", { camelCase: false, equalOperator: ":", separator: "," }));
